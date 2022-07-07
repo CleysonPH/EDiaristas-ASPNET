370b6c7a5ee2fd9c41cef1e4eb1a1d50727801aa
Cleyson Lima <cleysonph@gmail.com>
2022-07-06 22:37:23 -0300

    update user model

--- Core/Models/Usuario.cs
+++ Core/Models/Usuario.cs
@@ -5,4 +5,8 @@ namespace EDiaristas.Core.Models;
 public class Usuario : IdentityUser<int>
 {
     public string NomeCompleto { get; set; } = string.Empty;
+    public string? Cpf { get; set; }
+    public DateTime? Nascimento { get; set; }
+    public double? Reputacao { get; set; }
+    public string? ChavePix { get; set; }
 }
